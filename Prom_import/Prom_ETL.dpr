1f0148dabe26e748017a208d8729bafdcb7a0466
pterik <pterik@gmail.com>
2018-09-17 00:08:37 +0300

    CSV file XLS not finished

---
+++ Prom_import/Prom_ETL.dpr
@@ -0,0 +1,14 @@
+program Prom_ETL;
+
+uses
+  Vcl.Forms,
+  Main in 'Main.pas' {FormMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFormMain, FormMain);
+  Application.Run;
+end.
